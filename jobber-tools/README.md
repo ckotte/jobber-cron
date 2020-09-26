7812b775ba49ec2c4504a8f2ba5f4c3b31129e69
Christian Kotte <christian.kotte@gmx.de>
2020-09-26 21:05:16 +0200

    Add rclone to manage files on cloud storage

--- jobber-tools/README.md
+++ jobber-tools/README.md
@@ -8,6 +8,7 @@ Includes:
 * rsync
 * pg_dump and pg_restore
 * git and mercurial
+* rclone
 
 # GPG Public Keys
 
@@ -33,3 +34,40 @@ $ docker run -d --name jobber \
 
 > Will compress and encrypt backup folder via gpgtar each day at 2am.
 > You can use the fingerprint, the long key ID or the short key ID as the public key ID
+
+# Rclone & Cloud Storage
+
+You can use the command line program [rclone](https://rclone.org) to manage files on cloud storage. Rclone supports many [cloud storage providers](https://rclone.org#providers). However, a dynamic configuration is only supported for Dropbox and Google Drive. Alternatively, a rclone configuration file can be mapped inside the container.
+
+Example (Dynamic configuration):
+
+~~~~
+$ docker run -d --name jobber \
+    -e "JOB_NAME1=rclone" \
+    -e "JOB_COMMAND1=rclone ls Dropbox: && rclone ls Google_Drive:" \
+    -e "JOB_TIME1=0 0 2 * * *" \
+    -e "JOB_ON_ERROR1=Continue" \
+    -e "RCLONE_DROPBOX_NAME=Dropbox"
+    -e "RCLONE_DROPBOX_TOKEN=iUbzKoSHcIsGGGAAAAAlilZVO31lKn9RQnc4uzMVbJEjn5WyLx1JnFI2xpLsHJDQ"
+    -e "RCLONE_DROPBOX_TOKEN_TYPE=bearer"
+    -e "RCLONE_DROPBOX_TOKEN_EXPIRY: "0001-01-01T00:00:00Z"
+    -e "RCLONE_GOOGLE_DRIVE_NAME=Google_Drive"
+    -e "RCLONE_GOOGLE_DRIVE_TOKEN=zb30.iUbzKoSHcIsGGGAAAAAlilZVO31lKn9RQnc4uzMVbJEjn5WyLx1JnFI2xpLsHJDQ"
+    -e "RCLONE_GOOGLE_DRIVE_TOKEN_TYPE=bearer"
+    -e "RCLONE_GOOGLE_DRIVE_REFRESH_TOKEN=2//iUbzKoSHcIsGGGAAAAAlilZVO31lKn9RQnc4uzMVbJEjn5WyLx1JnFI2xpLsHJDQ"
+    -e "RCLONE_GOOGLE_DRIVE_TOKEN_EXPIRY=2020-08-17T18:03:42.301764255+02:00"
+    -e "RCLONE_GOOGLE_DRIVE_ROOT_FOLDER_ID=0AEbE71WbqwYMUk9QVA"
+    ckotte/jobber:tools
+~~~~
+
+Example (Existing configuration file):
+
+~~~~
+$ docker run -d --name jobber \
+    -v "/rclone/config/directory:/root/.config/rclone"
+    -e "JOB_NAME1=rclone" \
+    -e "JOB_COMMAND1=rclone ls Dropbox:" \
+    -e "JOB_TIME1=0 0 2 * * *" \
+    -e "JOB_ON_ERROR1=Continue" \
+    ckotte/jobber:tools
+~~~~
--- jobber-tools/imagescripts/docker-entrypoint.sh
+++ jobber-tools/imagescripts/docker-entrypoint.sh
@@ -4,24 +4,60 @@ set -o errexit
 
 [[ ${DEBUG} == true ]] && set -x
 
-if [ ! -f "/root/.gnupg/pubring.gpg" ]; then
-  if [ -n "${GPG_PRIVATE_KEY}" ]; then
-    gpg --allow-secret-key-import --import ${GPG_PRIVATE_KEY}
-  fi
+if [ "$EUID" -eq 0 ]; then
+  mkdir -p /root/.config/rclone/
+  rclone_configfile="/root/.config/rclone/rclone.conf"
+else
+  mkdir -p /home/$(whoami)/.config/rclone/
+  rclone_configfile="/home/$(whoami)/.config/rclone/rclone.conf"
+fi
 
-  if [ -n "${GPG_PUBLIC_KEY}" ]; then
-    gpg --import ${GPG_PUBLIC_KEY}
-    if [ "${AUTO_TRUST_GPG_PUBLIC_KEY}" = "true" ]; then
-      # gpg1
-      # ID=$(keyVal=$(gpg --list-keys | awk '/pub/{if (length($2) > 0) print $2}'); echo "${keyVal##*/}")
-      # echo "$( gpg --list-keys --fingerprint \
-      #   | grep $ID -A 1 | tail -1 \
-      #   | tr -d '[:space:]' | awk 'BEGIN { FS = "=" } ; { print $2 }' \
-      # ):6:" | gpg --import-ownertrust &> /dev/null;
-      # gpg2
-      gpg --export-ownertrust && echo $GPG_PUBLIC_KEY_ID:6: | gpg --import-ownertrust
-    fi
+if [ -n "${GPG_PRIVATE_KEY}" ]; then
+  gpg --allow-secret-key-import --import ${GPG_PRIVATE_KEY}
+fi
+
+if [ -n "${GPG_PUBLIC_KEY}" ]; then
+  gpg --import ${GPG_PUBLIC_KEY}
+  if [ "${AUTO_TRUST_GPG_PUBLIC_KEY}" = "true" ]; then
+    # gpg1
+    # ID=$(keyVal=$(gpg --list-keys | awk '/pub/{if (length($2) > 0) print $2}'); echo "${keyVal##*/}")
+    # echo "$( gpg --list-keys --fingerprint \
+    #   | grep $ID -A 1 | tail -1 \
+    #   | tr -d '[:space:]' | awk 'BEGIN { FS = "=" } ; { print $2 }' \
+    # ):6:" | gpg --import-ownertrust &> /dev/null;
+    # gpg2
+    gpg --export-ownertrust && echo $GPG_PUBLIC_KEY_ID:6: | gpg --import-ownertrust
   fi
+fi
+
+if [ -n "${RCLONE_DROPBOX_NAME}" ] && \
+   [ -n "${RCLONE_DROPBOX_TOKEN}" ] && \
+   [ -n "${RCLONE_DROPBOX_TOKEN_TYPE}" ] && \
+   [ -n "${RCLONE_DROPBOX_TOKEN_EXPIRY}" ]; then
+  cat >> ${rclone_configfile} <<_EOF_
+[${RCLONE_DROPBOX_NAME}]
+type = dropbox
+client_id =
+client_secret =
+token = {"access_token":"${RCLONE_DROPBOX_TOKEN}","token_type":"${RCLONE_DROPBOX_TOKEN_TYPE}","expiry":"${RCLONE_DROPBOX_TOKEN_EXPIRY}"}
+
+_EOF_
+fi
+
+if [ -n "${RCLONE_GOOGLE_DRIVE_NAME}" ] && \
+   [ -n "${RCLONE_GOOGLE_DRIVE_TOKEN}" ] && \
+   [ -n "${RCLONE_GOOGLE_DRIVE_TOKEN_TYPE}" ] && \
+   [ -n "${RCLONE_GOOGLE_DRIVE_REFRESH_TOKEN}" ] && \
+   [ -n "${RCLONE_GOOGLE_DRIVE_TOKEN_EXPIRY}" ] && \
+   [ -n "${RCLONE_GOOGLE_DRIVE_ROOT_FOLDER_ID}" ]; then
+  cat >> ${rclone_configfile} <<_EOF_
+[${RCLONE_GOOGLE_DRIVE_NAME}]
+type = drive
+scope = drive
+token = {"access_token":"${RCLONE_GOOGLE_DRIVE_TOKEN}","token_type":"${RCLONE_GOOGLE_DRIVE_TOKEN_TYPE}","refresh_token":"${RCLONE_GOOGLE_DRIVE_REFRESH_TOKEN}","expiry":"${RCLONE_GOOGLE_DRIVE_TOKEN_EXPIRY}"}
+root_folder_id = ${RCLONE_GOOGLE_DRIVE_ROOT_FOLDER_ID}
+
+_EOF_
 fi
 
 /opt/jobber/docker-entrypoint.sh "$@"
